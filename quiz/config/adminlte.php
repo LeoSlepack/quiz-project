98f7a02c23614f67afeb385bb2f7189a314576a1
Leonardo Slepack <111914236+LeoSlepack@users.noreply.github.com>
2023-02-09 09:09:07 -0300

    button
    - create button search

--- quiz/config/adminlte.php
+++ quiz/config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'Login Quiz',
+    'title' => 'AdminLTE 3',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => 'Login Quiz',
+    'logo' => '<b>Admin</b>LTE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
